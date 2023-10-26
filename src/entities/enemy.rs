13eec51d9056ef0e31f4c3666c4903d2d987b872
Rayato159 <rayato159@gmail.com>
2023-10-27 00:33:39 +0700

    -feature :sparkles: add enemy rand_x and rand_y bound

--- src/entities/enemy.rs
+++ src/entities/enemy.rs
@@ -14,8 +14,9 @@ pub fn spawn_enemies(
 ) {
     for (window, _) in window_query.iter() {
         for _ in 0..NUMBER_OF_ENEMIES {
-            let rand_x = random::<f32>() * window.width();
-            let rand_y = random::<f32>() * window.height();
+
+            let rand_x = get_enemy_rand_x(window);
+            let rand_y = get_enemy_rand_y(window);
     
             commands.spawn((
                 SpriteBundle {
@@ -28,5 +29,39 @@ pub fn spawn_enemies(
                 },
             ));
         }
+    }
+}
+
+pub fn get_enemy_rand_x(window: &Window) -> f32 {
+    let rand_x = random::<f32>() * window.width();
+
+    let half_enemy_size = ENEMY_SIZE / 2.;
+
+    let x_min = 0.0 + half_enemy_size;
+    let x_max = window.width() - half_enemy_size;
+    
+    if rand_x < x_min {
+        x_min
+    } else if rand_x > x_max {
+        x_max
+    } else {
+        rand_x
+    }
+}
+
+pub fn get_enemy_rand_y(window: &Window) -> f32 {
+    let rand_y = random::<f32>() * window.height();
+
+    let half_enemy_size = ENEMY_SIZE / 2.;
+
+    let y_min = 0.0 + half_enemy_size;
+    let y_max = window.height() - half_enemy_size;
+    
+    if rand_y < y_min {
+        y_min
+    } else if rand_y > y_max {
+        y_max
+    } else {
+        rand_y
     }
 }
